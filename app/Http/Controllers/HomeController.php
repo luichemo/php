f3182591eb3b0b62f9843fb2f68d6091aa6a6fa6
luichemo <luka.chemia001@gmail.com>
2024-01-14 18:34:31 +0400

    project-is-ready

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quiz;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(): View
+    {
+        $query = Quiz::withCount(['questions'])
+            ->has('questions')
+            ->when(auth()->guest() || ! auth()->user()->is_admin, function($query) {
+                return $query->where('published', 1);
+            })
+            ->orderBy('id')
+            ->get();
+
+        $public = $query->where('public', true);
+        $registered = $query->where('public', false);
+
+        return view('home', compact('public', 'registered'));
+    }
+
+    public function show(Quiz $quiz, $slug = null)
+    {
+        return view('front.quizzes.show', compact( 'quiz'));
+    }
+}
---
+++ app/Http/Livewire/Quiz/QuizForm.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Livewire\Quiz;
+
+use App\Models\Question;
+use App\Models\Quiz;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+use Illuminate\Support\Str;
+use Livewire\Redirector;
+
+class QuizForm extends Component
+{
+    public Quiz $quiz;
+
+    public bool $editing = false;
+
+    public array $questions = [];
+
+    public array $listsForFields = [];
+
+    public function mount(Quiz $quiz): void
+    {
+        $this->quiz = $quiz;
+
+        $this->initListsForFields();
+
+        if ($this->quiz->exists) {
+            $this->editing = true;
+
+            $this->questions = $this->quiz->questions()->pluck('id')->toArray();
+        } else {
+            $this->quiz->published = false;
+            $this->quiz->public = false;
+        }
+    }
+
+    public function updatedQuizTitle(): void
+    {
+        $this->quiz->slug = Str::slug($this->quiz->title);
+    }
+
+    public function save(): Redirector
+    {
+        $this->validate();
+
+        $this->quiz->save();
+
+        $this->quiz->questions()->sync($this->questions);
+
+        return to_route('quizzes');
+    }
+
+    public function render(): View
+    {
+        return view('livewire.quiz.form');
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'quiz.title'       => [
+                'string',
+                'required',
+            ],
+            'quiz.slug'        => [
+                'string',
+                'nullable',
+            ],
+            'quiz.description' => [
+                'string',
+                'nullable',
+            ],
+            'quiz.published'   => [
+                'boolean',
+            ],
+            'quiz.public'      => [
+                'boolean',
+            ],
+        ];
+    }
+
+    protected function initListsForFields(): void
+    {
+        $this->listsForFields['questions'] = Question::pluck('question_text', 'id')->toArray();
+    }
+
+
+}
